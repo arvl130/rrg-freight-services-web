ed3034bf23e1edbf50fc9dbb065a1f8bfe7f338b
Angelo Geulin <angelogeulin123@gmail.com>
2023-09-23 17:49:44 +0800

    feat: configure default font and brand colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,20 @@ import "@/styles/globals.css"
 import { api } from "@/utils/api"
 import { AuthProvider } from "@/utils/auth"
 import type { AppProps } from "next/app"
+import { DM_Sans } from "next/font/google"
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  weight: ["400", "500", "600", "700"],
+})
 
 export default api.withTRPC(({ Component, pageProps }: AppProps) => {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <div className={`${dmSans.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   )
 })
