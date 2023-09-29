5ec8679b7a5c4b03d9c2b8383621d87a791c7a84
Angelo Geulin <angelogeulin123@gmail.com>
2023-09-29 15:36:49 +0800

    feat: add header to admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,8 +1,15 @@
+import { useSession } from "@/utils/auth"
 import {
+  Bell,
+  CaretDown,
   Gauge,
+  Gear,
+  List,
+  MagnifyingGlass,
   Package,
   Scroll,
   SignOut,
+  UserCircle,
   UsersThree,
 } from "@phosphor-icons/react"
 import { getAuth, signOut } from "firebase/auth"
@@ -21,6 +28,7 @@ export function AdminLayout({
 }) {
   const router = useRouter()
   const [isSigningOut, setIsSigningOut] = useState(false)
+  const { user } = useSession()
 
   return (
     <>
@@ -123,7 +131,46 @@ export function AdminLayout({
             </button>
           </div>
         </div>
-        <div className="bg-brand-cyan-100 px-4 py-2">{children}</div>
+        <div className="bg-brand-cyan-100 px-6 py-4">
+          <header className="flex justify-between bg-white px-6 py-4 rounded-lg shadow-md shadow-brand-cyan-500 mb-4">
+            <div className="flex items-center gap-3 rounded-md">
+              <div>
+                <List size={24} />
+              </div>
+              <div className="relative">
+                <input
+                  type="text"
+                  className="text-sm w-full pl-8 pr-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+                />
+                <MagnifyingGlass className="absolute left-3 top-3" />
+              </div>
+            </div>
+            <div className="flex">
+              <select className="block text-sm font-medium pl-8 pr-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring">
+                <option value="en-US">English</option>
+              </select>
+              <div className="px-2 flex">
+                <button type="button" className="px-2">
+                  <Gear size={24} />
+                </button>
+                <button type="button" className="px-2">
+                  <Bell size={24} />
+                </button>
+                <button
+                  type="button"
+                  className="px-2 py-2 whitespace-nowrap flex gap-2 items-center text-sm w-40 text-gray-700"
+                >
+                  <UserCircle size={24} />
+                  <div className="flex items-center gap-2">
+                    <span>{user?.displayName}</span>
+                    <CaretDown size={12} />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </header>
+          <div>{children}</div>
+        </div>
       </div>
     </>
   )
