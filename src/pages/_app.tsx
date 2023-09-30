4e1f461c7c587abf930fabc06d1d8297762ded03
Angelo Geulin <angelogeulin123@gmail.com>
2023-09-30 23:30:25 +0800

    feat: support font weight 800, 900 in DM Sans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { DM_Sans } from "next/font/google"
 const dmSans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-dm-sans",
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 })
 
 export default api.withTRPC(({ Component, pageProps }: AppProps) => {
