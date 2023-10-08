3d06cb1fe95217f34de527204b2d1ff37259bc82
Angelo Geulin <angelogeulin123@gmail.com>
2023-10-08 22:08:17 +0800

    feat: cache db instance during development

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,13 +1,21 @@
 import mysql from "mysql2/promise"
 import { serverEnv } from "../env"
-import { drizzle } from "drizzle-orm/mysql2"
+import { MySql2Database, drizzle } from "drizzle-orm/mysql2"
 import * as schema from "./schema"
 
 const poolConnection = mysql.createPool({
   uri: serverEnv.DATABASE_URL,
 })
 
-export const db = drizzle(poolConnection, {
-  mode: serverEnv.DATABASE_MODE,
-  schema,
-})
+const globalForDrizzle = globalThis as unknown as {
+  db: MySql2Database<typeof schema>
+}
+
+export const db =
+  globalForDrizzle.db ||
+  drizzle(poolConnection, {
+    mode: serverEnv.DATABASE_MODE,
+    schema,
+  })
+
+if (process.env.NODE_ENV !== "production") globalForDrizzle.db = db
