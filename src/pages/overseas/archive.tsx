a83cf1238ac3ad99cacca2bf7fbfabc78fc7f7bd
Yumeko <arlenejoy.nacion777@gmail.com>
2023-10-09 10:10:56 +0800

    feat: update pages: archive, packages & shipments

--- src/pages/overseas/archive.tsx
+++ src/pages/overseas/archive.tsx
@@ -1,23 +1,24 @@
-import { OverseasLayout } from "@/layouts/overseas"
-import { useSession } from "@/utils/auth" 
+import { useSession } from "@/utils/auth"
+import { OverseasLayout } from "@/layouts/overseas" 
+import { DownloadSimple } from "@phosphor-icons/react/DownloadSimple"
 import { DotsThree } from "@phosphor-icons/react/DotsThree"
-import { Export } from "@phosphor-icons/react/Export"
+import { Printer } from "@phosphor-icons/react/Printer"
 import { UserCircle } from "@phosphor-icons/react/UserCircle"
 import { MagnifyingGlass } from "@phosphor-icons/react/MagnifyingGlass"
 import { CaretLeft } from "@phosphor-icons/react/CaretLeft"
 import { CaretDoubleLeft } from "@phosphor-icons/react/CaretDoubleLeft"
 import { CaretRight } from "@phosphor-icons/react/CaretRight"
 import { CaretDoubleRight } from "@phosphor-icons/react/CaretDoubleRight"
-
+import { Export } from "@phosphor-icons/react/Export"
 function PageHeader() {
   return (
     <div className="flex justify-between mb-6">
-      <h1 className="text-3xl font-black [color:_#00203F]">Cluster List</h1>
+      <h1 className="text-3xl font-black [color:_#00203F]">Archive Packages</h1>
     </div>
   )
 }
 
-const shipments = [
+const packages = [
   {
     id: 1000000,
     sender: {
@@ -28,9 +29,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-     
+    status: "Preparing",
   },
   {
     id: 1000001,
@@ -42,13 +41,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "Shipped Out",
   },
   {
     id: 1000002,
@@ -60,13 +53,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "In Warehouse",
   },
   {
     id: 1000003,
@@ -78,13 +65,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "Prepared by Agent",
   },
   {
     id: 1000004,
@@ -96,13 +77,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "Delivered",
   },
   {
     id: 1000005,
@@ -114,13 +89,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "Delivered",
   },
   {
     id: 1000006,
@@ -132,13 +101,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-    action: {
-      view: "View",
-      edit: "Edit",
-      archieve: "Archieve",
-      },
+    status: "Out for Delivery",
   },
   {
     id: 1000007,
@@ -150,9 +113,7 @@ const shipments = [
       name: "John Dela Cruz",
       address: "25 Brgy. Gulod Novaliches Quezon City",
     },
-    date: "2023-10-01 10:15",
-    shipped: "Ship Freight",
-     
+    status: "Delivered",
   },
 ]
 
@@ -166,7 +127,7 @@ export default function UsersPage() {
   if (isLoading || role !== "OVERSEAS_AGENT") return <>...</>
 
   return (
-    <OverseasLayout title="Shipments">
+    <OverseasLayout title="Packages">
       <PageHeader />
       <div className="flex justify-between gap-3 bg-white px-6 py-4 rounded-lg shadow-md shadow-brand-cyan-500 mb-6">
         <div className="grid grid-cols-[1fr_2.25rem] h-[2.375rem]">
@@ -183,29 +144,51 @@ export default function UsersPage() {
             <MagnifyingGlass size={16} />
           </button>
         </div>
-       
+        <div className="flex gap-3 text-sm">
+          <select className="bg-white border border-gray-300 px-2 py-1.5 w-32 rounded-md text-gray-400 font-medium">
+            <option value="">Status</option>
+          </select>
+          <select className="bg-white border border-gray-300 px-2 py-1.5 w-32 rounded-md text-gray-400 font-medium">
+            <option value="">Warehouse</option>
+          </select>
+          <select className="bg-white border border-gray-300 px-2 py-1.5 w-32 rounded-md text-gray-400 font-medium">
+            <option value="">City</option>
+          </select>
+          <button
+            type="button"
+            className="bg-white border border-gray-300 px-3 py-1.5 rounded-md text-gray-400 font-medium"
+          >
+            Clear Filter
+          </button>
+        </div>
         <div className="flex gap-3 text-sm">
           <button
             type="button"
             className="flex items-center gap-1 bg-brand-cyan-500 text-white px-6 py-2 font-medium"
-          > 
-            <span>Create New</span>
+          >
+            <Export size={16} />
+            <span>Export</span>
           </button>
           <button
             type="button"
             className="flex items-center gap-1 bg-brand-cyan-500 text-white px-6 py-2 font-medium"
-          > 
-            <span>Archived Cluster</span>
+          >
+            <Printer size={16} />
+            <span>Print</span>
           </button>
         </div>
       </div>
       <div className="bg-white px-6 py-4 rounded-lg shadow-md shadow-brand-cyan-500 min-h-[36rem]">
         <div className="flex justify-between mb-3">
           <div className="flex gap-6">
-            <h2 className="text-2xl font-semibold text-brand-cyan-500 pb-1 border-b-2 border-brand-cyan-500">
-              All Clusters
-            </h2>
-           
+            <a 
+            href="./packages"
+            className="text-2xl text-gray-400 pb-1  ">
+              All Packages
+            </a>
+            <button className="text-2xl font-semibold border-b-2  text-brand-cyan-500 border-brand-cyan-500 pb-1">
+              Archived Packages
+            </button>
           </div>
           <div className="flex gap-8">
             <div>
@@ -245,56 +228,63 @@ export default function UsersPage() {
         {/* Table */}
         <div>
           {/* Header */}
-          <div className="grid grid-cols-6 border-y border-gray-300 font-medium">
-            <div className="uppercase px-4  py-2 flex gap-2">
+          <div className="grid grid-cols-4 border-y border-gray-300 font-medium">
+            <div className="uppercase px-4 py-2 flex gap-1">
               <input type="checkbox" name="" id="" />
-              <span>Cluster ID</span>
+              <span>Product ID</span>
             </div>
-            <div className="uppercase px-4  py-2">Sender Agent </div>
-            <div className="uppercase px-4  py-2">Receiver Agent</div>
-            <div className="uppercase px-4  py-2">Date Added</div>
-            <div className="uppercase px-4  py-2">Shipped Via</div>
-            <div className="uppercase px-12  py-2">Action</div>
+            <div className="uppercase px-4 py-2">Sender</div>
+            <div className="uppercase px-4 py-2">Receiver</div>
+            <div className="uppercase px-4 py-2">Status</div>
           </div>
           {/* Body */}
           <div>
-            {shipments.map((_shipment) => (
+            {packages.map((_package) => (
               <div
-                key={_shipment.id}
-                className="grid grid-cols-6 border-b border-gray-300 text-sm"
+                key={_package.id}
+                className="grid grid-cols-4 border-b border-gray-300 text-sm"
               >
-                <div className="px-4 py-2 flex items-center gap-2  ">
+                <div className="px-4 py-2 flex items-center gap-1">
                   <input type="checkbox" name="" id="" />
-                  <span>{_shipment.id}</span>
+                  <span>{_package.id}</span>
                 </div>
                 <div className="px-4 py-2">
-                  <div>{_shipment.sender.name}</div>
-                  <div className="text-gray-400">{_shipment.sender.address}</div>
+                  <div>{_package.sender.name}</div>
+                  <div className="text-gray-400">{_package.sender.address}</div>
                 </div>
-                <div className=" px-4 py-2">
-                  <div>{_shipment.receiver.name}</div>
+                <div className="px-4 py-2">
+                  <div>{_package.receiver.name}</div>
                   <div className="text-gray-400">
-                    {_shipment.receiver.address}
+                    {_package.receiver.address}
                   </div>
                 </div>
-                <div className="px-4 py-2">
-                  <div>{_shipment.date}</div> 
+                <div className="px-4 py-2 flex items-center gap-2">
+                  <div
+                    className={`
+                      w-36 py-0.5 text-white text-center rounded-md
+                      ${_package.status === "Preparing" ? "bg-gray-400" : ""}
+                      ${_package.status === "Shipped Out" ? "bg-blue-500" : ""}
+                      ${_package.status === "In Warehouse" ? "bg-pink-500" : ""}
+                      ${
+                        _package.status === "Prepared by Agent"
+                          ? "bg-pink-500"
+                          : ""
+                      }
+                      ${_package.status === "Delivered" ? "bg-green-500" : ""}
+                      ${
+                        _package.status === "Out for Delivery"
+                          ? "bg-orange-500"
+                          : ""
+                      }
+                  `}
+                  >
+                    {_package.status}
+                  </div>
+                  <button type="button">
+                    <span className="sr-only">Actions</span>
+                    <DotsThree size={16} />
+                  </button>
                 </div>
-                <div className="px-4 py-2">
-                  <div>{_shipment.shipped}</div> 
-                </div>
-                <div className="flex gap-2">
-                <div className="  py-2">
-                  <button className="bg-[#4B61D7] text-white text-md rounded-md text-center p-2">View</button > 
-                </div>
-                <div className=" py-2">
-                  <button className="bg-[#65DB7F] text-white text-md rounded-md text-center p-2">Edit</button > 
-                </div>
-                <div className="  py-2">
-                  <button className="bg-[#D64D4D] text-white text-md rounded-md text-center p-2">Archive</button > 
-                </div>
-                </div>
-                
               </div>
             ))}
           </div>
