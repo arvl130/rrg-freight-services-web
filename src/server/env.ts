531f497c93fd20e44cd30685ae6673368c4b25a4
Angelo Geulin <angelogeulin123@gmail.com>
2023-10-21 14:53:30 +0800

    feat: add test users to env

--- src/server/env.ts
+++ src/server/env.ts
@@ -8,6 +8,10 @@ export const serverEnv = createEnv({
     FIREBASE_ADMIN_PROJECT_ID: z.string().min(1),
     FIREBASE_ADMIN_PRIVATE_KEY: z.string().min(1),
     FIREBASE_ADMIN_CLIENT_EMAIL: z.string().min(1),
+    TEST_USER_ID_ADMIN: z.string().length(28),
+    TEST_USER_ID_WAREHOUSE: z.string().length(28),
+    TEST_USER_ID_OVERSEAS: z.string().length(28),
+    TEST_USER_ID_DOMESTIC: z.string().length(28),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
@@ -15,5 +19,9 @@ export const serverEnv = createEnv({
     FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
     FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
     FIREBASE_ADMIN_CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    TEST_USER_ID_ADMIN: process.env.TEST_USER_ID_ADMIN,
+    TEST_USER_ID_WAREHOUSE: process.env.TEST_USER_ID_WAREHOUSE,
+    TEST_USER_ID_OVERSEAS: process.env.TEST_USER_ID_OVERSEAS,
+    TEST_USER_ID_DOMESTIC: process.env.TEST_USER_ID_DOMESTIC,
   },
 })
