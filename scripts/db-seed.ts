68e9f80a0dee4481835324eff463af4effe1fc8d
Angelo Geulin <angelogeulin123@gmail.com>
2023-10-22 14:05:48 +0800

    chore: update db seed script

--- scripts/db-seed.ts
+++ scripts/db-seed.ts
@@ -3,7 +3,14 @@ import { serverEnv } from "@/server/env"
 import * as schema from "@/server/db/schema"
 import mysql from "mysql2/promise"
 import { drizzle } from "drizzle-orm/mysql2"
-import { NewPackage, NewPackageStatusLog } from "@/server/db/entities"
+import {
+  NewUser,
+  NewPackage,
+  NewPackageStatusLog,
+  NewShipment,
+  NewShipmentHub,
+  NewShipmentStatusLog,
+} from "@/server/db/entities"
 import { DateTime } from "luxon"
 
 const pool = mysql.createPool({
@@ -15,10 +22,48 @@ const db = drizzle(pool, {
   schema,
 })
 
+const newUsers: NewUser[] = [
+  {
+    id: serverEnv.TEST_ADMIN_USER_ID,
+    displayName: "Administrator",
+    contactNumber: "+639123456789",
+    emailAddress: serverEnv.TEST_ADMIN_EMAIL,
+    role: "ADMIN",
+    gender: "MALE",
+    isEnabled: 1,
+  },
+  {
+    id: serverEnv.TEST_WAREHOUSE_USER_ID,
+    displayName: "Warehouse Staff",
+    contactNumber: "+639987654321",
+    emailAddress: serverEnv.TEST_WAREHOUSE_EMAIL,
+    role: "WAREHOUSE",
+    gender: "MALE",
+    isEnabled: 1,
+  },
+  {
+    id: serverEnv.TEST_DOMESTIC_USER_ID,
+    displayName: "Domestic Agent",
+    contactNumber: "+639112223333",
+    emailAddress: serverEnv.TEST_DOMESTIC_EMAIL,
+    role: "DOMESTIC_AGENT",
+    gender: "MALE",
+    isEnabled: 1,
+  },
+  {
+    id: serverEnv.TEST_OVERSEAS_USER_ID,
+    displayName: "Overseas Agent",
+    contactNumber: "+1112223333",
+    emailAddress: serverEnv.TEST_OVERSEAS_EMAIL,
+    role: "OVERSEAS_AGENT",
+    gender: "FEMALE",
+    isEnabled: 1,
+  },
+]
+
 const newPackages: NewPackage[] = [
   {
     id: 10_000,
-    shipmentId: 20_000,
     shippingParty: "FIRST_PARTY",
     shippingMode: "SEA",
     shippingType: "STANDARD",
@@ -41,8 +86,8 @@ const newPackages: NewPackage[] = [
     receiverStateOrProvince: "National Capital Region",
     receiverCountryCode: "PHL",
     receiverPostalCode: 2222,
-    createdById: serverEnv.TEST_USER_ID_WAREHOUSE,
-    updatedById: serverEnv.TEST_USER_ID_WAREHOUSE,
+    createdById: serverEnv.TEST_OVERSEAS_USER_ID,
+    updatedById: serverEnv.TEST_OVERSEAS_USER_ID,
   },
 ]
 
@@ -53,6 +98,16 @@ const dateFiveMinsAgo = now
     minutes: 5,
   })
   .toJSDate()
+const dateTenMinsAgo = now
+  .minus({
+    minutes: 10,
+  })
+  .toJSDate()
+const dateFifteenMinsAgo = now
+  .minus({
+    minutes: 15,
+  })
+  .toJSDate()
 
 const newPackageStatusLogs: NewPackageStatusLog[] = [
   {
@@ -60,19 +115,86 @@ const newPackageStatusLogs: NewPackageStatusLog[] = [
     packageId: 10_000,
     status: "PENDING",
     description: "Package created.",
-    createdAt: dateFiveMinsAgo,
+    createdAt: dateFifteenMinsAgo,
+    createdById: serverEnv.TEST_OVERSEAS_USER_ID,
   },
   {
     id: 30_001,
     packageId: 10_000,
     status: "PREPARED_BY_AGENT",
     description: "Package has been prepared by agent.",
-    createdAt: dateNow,
+    createdAt: dateTenMinsAgo,
+    createdById: serverEnv.TEST_OVERSEAS_USER_ID,
   },
 ]
 
+const newShipmentHubs: NewShipmentHub[] = [
+  {
+    id: 1,
+    displayName: "UAE Hub",
+    streetAddress: "Villa 5 31st St.",
+    city: "Dubai",
+    stateOrProvince: "Dubai",
+    countryCode: "ARE",
+    postalCode: 5555,
+    role: "SENDING",
+  },
+  {
+    id: 2,
+    displayName: "Main Hub",
+    streetAddress: "B1 L2 Sampaguita St.",
+    city: "Quezon City",
+    stateOrProvince: "National Capital Region",
+    countryCode: "PHL",
+    postalCode: 6666,
+    role: "SENDING_RECEIVING",
+  },
+  {
+    id: 3,
+    displayName: "Visayas Hub",
+    streetAddress: "B5 L6 Kalachuchi St.",
+    city: "Cebu City",
+    stateOrProvince: "Cebu",
+    countryCode: "PHL",
+    postalCode: 7777,
+    role: "RECEIVING",
+  },
+]
+
+const newShipments: NewShipment[] = [
+  {
+    id: 20_000,
+    originHubId: 1,
+    destinationHubId: 2,
+  },
+]
+
+const newShipmentsStatusLogs: NewShipmentStatusLog[] = [
+  {
+    id: 30_000,
+    shipmentId: 20_000,
+    status: "PENDING",
+    description: "Shipment created.",
+    createdAt: dateFiveMinsAgo,
+    createdById: serverEnv.TEST_OVERSEAS_USER_ID,
+  },
+  {
+    id: 30_001,
+    shipmentId: 20_000,
+    status: "PREPARED_BY_AGENT",
+    description: "Shipment has been prepared by agent.",
+    createdAt: dateNow,
+    createdById: serverEnv.TEST_OVERSEAS_USER_ID,
+  },
+]
+
+await db.insert(schema.users).values(newUsers)
 await db.insert(schema.packages).values(newPackages)
 await db.insert(schema.packageStatusLogs).values(newPackageStatusLogs)
+await db.insert(schema.shipmentHubs).values(newShipmentHubs)
+await db.insert(schema.shipments).values(newShipments)
+await db.insert(schema.shipmentStatusLogs).values(newShipmentsStatusLogs)
+
 await pool
   .end()
   .then(() => console.log("[\x1b[32m%s\x1b[0m] Database seeded", "✓"))
