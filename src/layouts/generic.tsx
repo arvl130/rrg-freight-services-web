8749f3c6f311910b4b6df6fab3db5232706521fe
Angelo Geulin <angelogeulin123@gmail.com>
2023-10-23 22:36:09 +0800

    refactor: move back layout header to same file

--- src/layouts/generic.tsx
+++ src/layouts/generic.tsx
@@ -6,6 +6,12 @@ import { AdminSideBar } from "./admin"
 import { WarehouseSideBar } from "./warehouse"
 import { DomesticSideBar } from "./domestic"
 import { OverseasSideBar } from "./overseas"
+import { List } from "@phosphor-icons/react/List"
+import { MagnifyingGlass } from "@phosphor-icons/react/MagnifyingGlass"
+import { Gear } from "@phosphor-icons/react/Gear"
+import { Bell } from "@phosphor-icons/react/Bell"
+import { UserCircle } from "@phosphor-icons/react/UserCircle"
+import { CaretDown } from "@phosphor-icons/react/CaretDown"
 import { Role } from "@/utils/constants"
 
 function GenericSidebar() {
@@ -47,10 +53,49 @@ export function GenericLayout({
       <div className="grid grid-cols-[4rem_minmax(0,_1fr)]">
         <GenericSidebar />
         <div className="bg-brand-cyan-100 px-6 py-4">
-          {children({
-            user,
-            role,
-          })}
+          <header className="flex justify-between bg-white px-6 py-4 rounded-lg shadow-md shadow-brand-cyan-500 mb-4">
+            <div className="flex items-center gap-3 rounded-md">
+              <div>
+                <List size={24} />
+              </div>
+              <div className="relative">
+                <input
+                  type="text"
+                  className="text-sm w-full pl-8 pr-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+                />
+                <MagnifyingGlass className="absolute left-3 top-3" />
+              </div>
+            </div>
+            <div className="flex">
+              <select className="block text-sm font-medium pl-8 pr-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring">
+                <option value="en-US">English</option>
+              </select>
+              <div className="px-2 flex text-gray-400">
+                <button type="button" className="px-2">
+                  <Gear size={24} />
+                </button>
+                <button type="button" className="px-2">
+                  <Bell size={24} />
+                </button>
+                <button
+                  type="button"
+                  className="px-2 py-2 whitespace-nowrap flex gap-2 items-center text-sm w-46 text-gray-700"
+                >
+                  <UserCircle size={24} />
+                  <div className="flex items-center gap-2">
+                    <span>{user.displayName}</span>
+                    <CaretDown size={12} />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </header>
+          <div>
+            {children({
+              user,
+              role,
+            })}
+          </div>
         </div>
       </div>
     </>
