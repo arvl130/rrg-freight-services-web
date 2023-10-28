ce3965275d742e66a964a164b6c22b4b18e6ece1
Angelo Geulin <angelogeulin123@gmail.com>
2023-10-28 14:39:45 +0800

    feat: enable update password form

--- src/pages/profile/change-password.tsx
+++ src/pages/profile/change-password.tsx
@@ -1,49 +1,116 @@
 import { GenericLayout } from "@/layouts/generic"
 import { ProfileSideNav } from "@/components/profile/sidenav"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  EmailAuthProvider,
+  User,
+  reauthenticateWithCredential,
+  updatePassword,
+} from "firebase/auth"
+import { useState } from "react"
 
-function RightColumn() {
+const updatePasswordFormSchema = z.object({
+  oldPassword: z.string().min(1),
+  newPassword: z.string().min(1),
+  newPasswordAgain: z.string().min(1),
+})
+
+type UpdatePasswordFormType = z.infer<typeof updatePasswordFormSchema>
+
+function UpdatePasswordForm({ user }: { user: User }) {
+  const [isUpdating, setIsUpdating] = useState(false)
+  const {
+    reset,
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<UpdatePasswordFormType>({
+    resolver: zodResolver(updatePasswordFormSchema),
+    defaultValues: {
+      oldPassword: "",
+      newPassword: "",
+      newPasswordAgain: "",
+    },
+  })
+
   return (
-    <article>
-      <form className="px-6 pt-4 pb-6 rounded-lg bg-white">
-        <div>
-          <h2 className="font-semibold pb-5">Change Password</h2>
+    <form
+      className="px-6 pt-4 pb-6 rounded-lg bg-white"
+      onSubmit={handleSubmit(async (formData) => {
+        setIsUpdating(true)
+        try {
+          const userCredential = await EmailAuthProvider.credential(
+            user.email!,
+            formData.oldPassword,
+          )
+
+          await reauthenticateWithCredential(user, userCredential)
+          await updatePassword(user, formData.oldPassword)
+          reset()
+        } finally {
+          setIsUpdating(false)
+        }
+      })}
+    >
+      <div>
+        <h2 className="font-semibold pb-5">Change Password</h2>
+      </div>
+      <div className="grid grid-rows-1">
+        <div className="mb-3">
+          <label className="block text-sm	text-gray-500 mb-1 ">
+            Current Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter current password"
+            className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+            {...register("oldPassword")}
+          />
         </div>
-        <div className="grid grid-rows-1">
-          <div className="mb-3">
-            <label className="block text-sm	text-gray-500 mb-1 ">
-              Enter Current Password
-            </label>
-            <input className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring" />
-          </div>
-          <div className="mb-3">
-            <label className="block text-sm	text-gray-500 mb-1 ">
-              Enter New Password
-            </label>
-            <input className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring" />
-          </div>
-          <div className="mb-5">
-            <label className="block text-sm	text-gray-500 mb-1 ">
-              Confirm New Password
-            </label>
-            <input className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring" />
-          </div>
+        <div className="mb-3">
+          <label className="block text-sm	text-gray-500 mb-1 ">
+            New Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter current password"
+            className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+            {...register("newPassword")}
+          />
         </div>
-        <button className="p-2 text-white	w-full bg-cyan-500 transition-colors hover:bg-cyan-400 rounded-lg font-medium">
-          Save
-        </button>
-      </form>
-    </article>
+        <div className="mb-5">
+          <label className="block text-sm	text-gray-500 mb-1 ">
+            Confirm New Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter current password again"
+            className="rounded-lg text-sm w-full px-4 py-2 text-gray-700 bg-white border border-cyan-500 focus:border-cyan-400 focus:ring-cyan-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+            {...register("newPasswordAgain")}
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        disabled={isUpdating}
+        className="p-2 text-white	w-full disabled:bg-cyan-300 bg-cyan-500 transition-colors hover:bg-cyan-400 rounded-lg font-medium"
+      >
+        {isUpdating ? "Saving ..." : "Save"}
+      </button>
+    </form>
   )
 }
 
 export default function ProfilePasswordPage() {
   return (
-    <GenericLayout title="Dashboard">
-      {() => (
+    <GenericLayout title="Dashboard" hasSession>
+      {({ user }) => (
         <main className="mt-6">
           <section className="grid grid-cols-[22rem_1fr] gap-6 max-w-4xl mx-auto">
             <ProfileSideNav />
-            <RightColumn />
+            <UpdatePasswordForm user={user} />
           </section>
         </main>
       )}
