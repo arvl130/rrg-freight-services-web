dbae9c6708a822fb50cc7e875347602846f146ee
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-04 16:38:14 +0800

    feat: add helper function for getting the entire shipment hub from user id

--- src/server/db/helpers/shipment-hub.ts
+++ src/server/db/helpers/shipment-hub.ts
@@ -57,3 +57,37 @@ export async function getShipmentHubIdOfUserId(
   const [{ shipmentHubId }] = shipmentHubsOfThisUser
   return shipmentHubId
 }
+
+export async function getShipmentHubOfUserId(
+  db: MySql2Database<typeof schema>,
+  userId: string,
+) {
+  const shipmentHubsOfThisUser = await db
+    .select()
+    .from(schema.shipmentHubAgents)
+    .innerJoin(
+      schema.shipmentHubs,
+      eq(schema.shipmentHubAgents.shipmentHubId, schema.shipmentHubs.id),
+    )
+    .where(eq(schema.shipmentHubAgents.userId, userId))
+
+  if (shipmentHubsOfThisUser.length === 0) {
+    throw new TRPCError({
+      code: "PRECONDITION_FAILED",
+      message: "Expected 1 shipment hub for this user, but got none",
+    })
+  }
+
+  if (shipmentHubsOfThisUser.length > 1) {
+    throw new TRPCError({
+      code: "PRECONDITION_FAILED",
+      message: "Expected 1 shipment hub for this user, but got more",
+    })
+  }
+
+  const [shipmentHub] = shipmentHubsOfThisUser.map(
+    ({ shipment_hubs }) => shipment_hubs,
+  )
+
+  return shipmentHub
+}
