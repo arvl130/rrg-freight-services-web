ca199fbfc0cb3957676f25ee22ccab96052f9b14
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-25 18:44:39 +0800

    fix: title

--- src/components/transfer-shipments/create-modal.tsx
+++ src/components/transfer-shipments/create-modal.tsx
@@ -22,7 +22,7 @@ export function TransferShipmentsCreateModal({
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[min(calc(100%_-_3rem),_56rem)] h-[32rem] grid grid-rows-[auto_1fr] rounded-2xl bg-white"
         >
           <Dialog.Title className="text-white font-bold text-center items-center py-2 [background-color:_#78CFDC] h-full rounded-t-2xl">
-            New Incoming Shipment
+            New Transfer Shipment
           </Dialog.Title>
           <div className="px-4 py-2 grid grid-rows-[auto_1fr]">wip</div>
         </Dialog.Content>
