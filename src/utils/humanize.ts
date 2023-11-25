5fe74382f2db85373d29d84bb77dc79b2cd33c2b
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-26 02:05:58 +0800

    feat: add humanize function for package status

--- src/utils/humanize.ts
+++ src/utils/humanize.ts
@@ -1,10 +1,20 @@
-import { Role } from "./constants"
+import { PackageStatus, Role } from "./constants"
 
 export function toTitleCase(word: string) {
   return word[0].toUpperCase() + word.slice(1).toLowerCase()
 }
 export function supportedRoleToHumanized(supportedRole: Role) {
   return supportedRole
+    .toLowerCase()
+    .split("_")
+    .map((word) => toTitleCase(word))
+    .join(" ")
+}
+
+export function supportedPackageStatusToHumanized(
+  packageStatus: PackageStatus,
+) {
+  return packageStatus
     .toLowerCase()
     .split("_")
     .map((word) => toTitleCase(word))
