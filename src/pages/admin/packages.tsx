63512926f3f7935d8d457d051db23214f275a435
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-25 18:16:12 +0800

    feat: make button invisible while it's still not implemented

--- src/pages/admin/packages.tsx
+++ src/pages/admin/packages.tsx
@@ -391,7 +391,7 @@ export default function PackagesPage() {
         <div className="flex gap-3 text-sm">
           <button
             type="button"
-            className="flex items-center gap-1 bg-brand-cyan-500 text-white px-6 py-2 font-medium"
+            className="flex items-center gap-1 bg-brand-cyan-500 text-white px-6 py-2 font-medium invisible"
             onClick={() => setIsOpenImportModal(true)}
           >
             <DownloadSimple size={16} />
