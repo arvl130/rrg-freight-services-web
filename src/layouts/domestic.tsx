ec6d389038f73eede7f239561bf3fa1e275f9a7e
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-28 00:51:01 +0800

    feat: add transfer shipments page in domestic agent user

--- src/layouts/domestic.tsx
+++ src/layouts/domestic.tsx
@@ -1,26 +1,23 @@
 import { useSession } from "@/utils/auth"
-import { Bell } from "@phosphor-icons/react/Bell"
-import { CaretDown } from "@phosphor-icons/react/CaretDown"
 import { Gauge } from "@phosphor-icons/react/Gauge"
-import { Gear } from "@phosphor-icons/react/Gear"
-import { List } from "@phosphor-icons/react/List"
-import { MagnifyingGlass } from "@phosphor-icons/react/MagnifyingGlass"
 import { Package } from "@phosphor-icons/react/Package"
 import { SignOut } from "@phosphor-icons/react/SignOut"
 import { UserCircle } from "@phosphor-icons/react/UserCircle"
-import { ClipboardText } from "@phosphor-icons/react/ClipboardText"
+import { Truck } from "@phosphor-icons/react/Truck"
 import { getAuth, signOut, User } from "firebase/auth"
 import Head from "next/head"
 import Image from "next/image"
 import { ReactNode, useState } from "react"
 import { SideBarLink } from "@/components/sidebar-link"
+import { AdminHeader, SkeletonAdminLayout } from "./admin"
+import { LoginPageHead, SkeletonLoginPage } from "@/pages/login"
 
 export function DomesticSideBar() {
   const [isSigningOut, setIsSigningOut] = useState(false)
 
   return (
     <nav className="bg-brand-cyan-500 flex flex-col justify-between items-center py-3 h-screen sticky top-0 bottom-0">
-      <div>
+      <div className="flex flex-col items-center gap-3 w-full">
         <Image
           src="/assets/img/logos/logo-white-bg.png"
           alt="RRG Freight Services circle logo with white background"
@@ -28,8 +25,6 @@ export function DomesticSideBar() {
           width={60}
           className="w-12 h-12 rounded-full"
         />
-      </div>
-      <div className="flex flex-col gap-3 w-full">
         <SideBarLink
           name="Dashboard"
           href="/domestic/dashboard"
@@ -42,8 +37,8 @@ export function DomesticSideBar() {
         />
         <SideBarLink
           name="Shipments"
-          href="/domestic/shipments"
-          icon={<ClipboardText size={32} className="text-white" />}
+          href="/domestic/transfer-shipments"
+          icon={<Truck size={32} className="text-white" />}
         />
         <SideBarLink
           name="Profile"
@@ -86,36 +81,83 @@ export function SkeletonDomesticLayout() {
   )
 }
 
-export function DomesticLayout({
-  title,
-  children,
-}: {
-  title: string
+type WithFunctionChildren = {
+  hasSession: true
   children: ({
     user,
     role,
   }: {
     user: User
     role: "DOMESTIC_AGENT"
+    reload: () => Promise<void>
   }) => ReactNode
-}) {
-  const { isLoading, user, role } = useSession({
+}
+
+type WithNodeChildren = {
+  hasSession?: false
+  children?: ReactNode
+}
+
+type LayoutProps = {
+  title: string | string[]
+} & (WithFunctionChildren | WithNodeChildren)
+
+export function DomesticLayout({ title, children }: LayoutProps) {
+  const titleContent = Array.isArray(title)
+    ? // Our deployment platform only supports Node v18.
+      // Use this method for now, until they support Node v20
+      // where toReversed() is available.
+      `${[...title].reverse().join(" \u2013 ")} \u2013 RRG Freight Services`
+    : `${title} \u2013 RRG Freight Services`
+
+  const { isLoading, user, role, reload } = useSession({
     required: {
       role: "DOMESTIC_AGENT",
     },
   })
 
-  // FIXME: Show skeleton for the login page in here.
-  if (isLoading || user === null)
-    return <main className="min-h-screen bg-brand-cyan-100"></main>
+  if (isLoading)
+    return (
+      <>
+        <Head>
+          <title>RRG Freight Services</title>
+          <meta
+            name="description"
+            content="RRG Freight Services is an international freight forwarding company. Contact us at +632 8461 6027 for any of your cargo needs."
+          />
+        </Head>
+        <main className="min-h-screen bg-brand-cyan-100"></main>
+      </>
+    )
+
+  if (user === null)
+    return (
+      <>
+        <LoginPageHead />
+        <SkeletonLoginPage />
+      </>
+    )
 
   // FIXME: Either use a unified skeleton component in here,
   // or show a skeleton component based on the detected role.
-  if (role !== "DOMESTIC_AGENT") return <SkeletonDomesticLayout />
+  if (role !== "DOMESTIC_AGENT")
+    return (
+      <>
+        <Head>
+          <title>Dashboard &#x2013; RRG Freight Services</title>
+          <meta
+            name="description"
+            content="RRG Freight Services is an international freight forwarding company. Contact us at +632 8461 6027 for any of your cargo needs."
+          />
+        </Head>
+        <SkeletonAdminLayout />
+      </>
+    )
+
   return (
     <>
       <Head>
-        <title>{title} &#x2013; RRG Freight Services</title>
+        <title>{titleContent}</title>
         <meta
           name="description"
           content="RRG Freight Services is an international freight forwarding company. Contact us at +632 8461 6027 for any of your cargo needs."
@@ -124,45 +166,28 @@ export function DomesticLayout({
       <div className="grid grid-cols-[4rem_minmax(0,_1fr)]">
         <DomesticSideBar />
         <div className="bg-brand-cyan-100 px-6 py-4">
-          <header className="flex justify-between bg-white px-6 py-4 rounded-lg shadow-md shadow-brand-cyan-500 mb-4">
-            <div className="flex items-center gap-3 rounded-md">
-              <div>
-                <List size={24} />
-              </div>
-              <div className="relative">
-                <input
-                  type="text"
-                  className="text-sm w-full pl-8 pr-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-                />
-                <MagnifyingGlass className="absolute left-3 top-3" />
-              </div>
-            </div>
-            <div className="flex">
-              <div className="px-2 flex text-gray-400">
-                <button type="button" className="px-2">
-                  <Gear size={24} />
-                </button>
-                <button type="button" className="px-2">
-                  <Bell size={24} />
-                </button>
-                <button
-                  type="button"
-                  className="px-2 py-2 whitespace-nowrap flex gap-2 items-center text-sm w-46 text-gray-700"
-                >
-                  <UserCircle size={24} />
-                  <div className="flex items-center gap-2">
-                    <span>{user?.displayName}</span>
-                    <CaretDown size={12} />
-                  </div>
-                </button>
-              </div>
-            </div>
-          </header>
+          <AdminHeader user={user} />
           <div>
-            {children({
-              user,
-              role,
-            })}
+            {typeof children === "function" ? (
+              <>
+                {/* When `children` is of type function, we can assume
+                    `hasSession` is also defined, so let's provide the
+                    session information.
+                */}
+                {children({
+                  user,
+                  role,
+                  reload,
+                })}
+              </>
+            ) : (
+              <>
+                {/* When `children` is not of type function, `hasSession`
+                    is not defined, so don't provide any session information.
+                */}
+                {children}
+              </>
+            )}
           </div>
         </div>
       </div>
