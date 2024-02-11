eead32650f1f0bcd14d87de4f51cde5884ad5369
Angelo Geulin <angelogeulin123@gmail.com>
2024-02-11 14:38:12 +0800

    fix: handle null path name

--- src/components/sidebar-link.tsx
+++ src/components/sidebar-link.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { useRouter } from "next/router"
+import { usePathname } from "next/navigation"
 import { ReactNode } from "react"
 
 export function SideBarLink({
@@ -13,7 +13,7 @@ export function SideBarLink({
   name: string
   icon: ReactNode
 }) {
-  const router = useRouter()
+  const pathname = usePathname()
   const matchingRouteNames = Array.isArray(otherRouteNames)
     ? [...new Set([href, ...otherRouteNames])]
     : [href]
@@ -24,9 +24,15 @@ export function SideBarLink({
       className={`
         flex justify-center items-center h-10 w-full hover:bg-sky-200 transition duration-200
         ${
-          matchingRouteNames.includes(router.pathname)
-            ? "border-x-2 border-l-white border-r-transparent"
-            : ""
+          pathname === null
+            ? ""
+            : `
+                ${
+                  matchingRouteNames.includes(pathname)
+                    ? "border-x-2 border-l-white border-r-transparent"
+                    : ""
+                }
+              `
         }
       `}
     >
