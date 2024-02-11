5a3f32ad9a2bfd93f3aea6469905fb990a807755
Angelo Geulin <angelogeulin123@gmail.com>
2024-02-11 20:45:36 +0800

    feat: use RSC for loading the data

--- src/app/admin/dashboard/packages-in-warehouse-tile.tsx
+++ src/app/admin/dashboard/packages-in-warehouse-tile.tsx
@@ -1,28 +1,23 @@
-"use client"
+import { db } from "@/server/db/client"
+import { packages } from "@/server/db/schema"
+import { Package } from "@phosphor-icons/react/dist/ssr/Package"
+import { count, eq } from "drizzle-orm"
+import { unstable_noStore as noStore } from "next/cache"
 
-import { api } from "@/utils/api"
-import { Package } from "@phosphor-icons/react/Package"
-
-export function PackagesInWarehouseTile() {
-  const { status, data } = api.package.getTotalPackageInWarehouse.useQuery()
+export async function PackagesInWarehouseTile() {
+  noStore()
+  const [{ value }] = await db
+    .select({
+      value: count(),
+    })
+    .from(packages)
+    .where(eq(packages.status, "IN_WAREHOUSE"))
 
   return (
-    <article
-      className="
-        text-[#29727C]
-        grid grid-cols-[1fr_6rem] shadow-md px-8 py-6 rounded-lg
-        bg-gradient-to-b from-[#79CFDCCC] to-[#79CFDC00]
-      "
-    >
+    <article className="text-[#29727C] grid grid-cols-[1fr_6rem] shadow-md px-8 py-6 rounded-lg bg-gradient-to-b from-[#79CFDCCC] to-[#79CFDC00]">
       <div className="flex flex-col justify-center items-start">
-        <>
-          <p className="text-4xl font-semibold">
-            {status === "loading" && <>...</>}
-            {status === "error" && <>error</>}
-            {status === "success" && <>{data.count}</>}
-          </p>
-          <p>Packages in-warehouse</p>
-        </>
+        <p className="text-4xl font-semibold">{value}</p>
+        <p>Packages in-warehouse</p>
       </div>
       <div>
         <Package size={96} />
