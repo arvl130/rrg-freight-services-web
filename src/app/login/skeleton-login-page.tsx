a7c2906a69401315a6b663ad429eaaf0d813df60
Angelo Geulin <angelogeulin123@gmail.com>
2024-02-16 01:29:27 +0800

    fix: incorrect bg color

--- src/app/login/skeleton-login-page.tsx
+++ src/app/login/skeleton-login-page.tsx
@@ -1,8 +1,3 @@
 export function SkeletonLoginPage() {
-  return (
-    <main className="min-h-screen grid grid-cols-2">
-      <div className="bg-cyan-600"></div>
-      <div className="bg-brand-cyan-450"></div>
-    </main>
-  )
+  return <main className="min-h-screen bg-brand-cyan-450" />
 }
