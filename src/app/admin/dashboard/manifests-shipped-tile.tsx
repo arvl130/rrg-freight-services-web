6e3fc96029ac0ba139e61125aa086f641839f307
Angelo Geulin <angelogeulin123@gmail.com>
2024-02-16 01:21:30 +0800

    refactor: migrate to RSC

--- src/app/admin/dashboard/manifests-shipped-tile.tsx
+++ src/app/admin/dashboard/manifests-shipped-tile.tsx
@@ -1,11 +1,37 @@
-"use client"
+import { db } from "@/server/db/client"
+import { shipments } from "@/server/db/schema"
+import { Article } from "@phosphor-icons/react/dist/ssr/Article"
+import { count, eq } from "drizzle-orm"
+import { unstable_noStore as noStore } from "next/cache"
 
-import { api } from "@/utils/api"
-import { Article } from "@phosphor-icons/react/Article"
+export async function SkeletonManifestsShippedTile() {
+  return (
+    <article
+      className="
+        text-[#AC873C]
+        grid grid-cols-[1fr_6rem] shadow-md px-8 py-6 rounded-lg
+        bg-gradient-to-b from-[#EDAD3E80] to-[#EDAD3E00]
+      "
+    >
+      <div className="flex flex-col justify-center items-start">
+        <p className="text-4xl font-semibold">...</p>
+        <p>Shipments shipped</p>
+      </div>
+      <div>
+        <Article size={96} />
+      </div>
+    </article>
+  )
+}
 
-export function ManifestsShippedTile() {
-  const { status, data } =
-    api.shipment.package.getTotalShipmentShipped.useQuery()
+export async function ManifestsShippedTile() {
+  noStore()
+  const [{ value }] = await db
+    .select({
+      value: count(),
+    })
+    .from(shipments)
+    .where(eq(shipments.status, "COMPLETED"))
 
   return (
     <article
@@ -16,11 +42,7 @@ export function ManifestsShippedTile() {
       "
     >
       <div className="flex flex-col justify-center items-start">
-        <p className="text-4xl font-semibold">
-          {status === "loading" && <>...</>}
-          {status === "error" && <>error</>}
-          {status === "success" && <>{data.count}</>}
-        </p>
+        <p className="text-4xl font-semibold">{value}</p>
         <p>Shipments shipped</p>
       </div>
       <div>
--- src/app/admin/dashboard/packages-in-warehouse-tile.tsx
+++ src/app/admin/dashboard/packages-in-warehouse-tile.tsx
@@ -4,6 +4,20 @@ import { Package } from "@phosphor-icons/react/dist/ssr/Package"
 import { count, eq } from "drizzle-orm"
 import { unstable_noStore as noStore } from "next/cache"
 
+export function SkeletonPackagesInWarehouseTile() {
+  return (
+    <article className="text-[#29727C] grid grid-cols-[1fr_6rem] shadow-md px-8 py-6 rounded-lg bg-gradient-to-b from-[#79CFDCCC] to-[#79CFDC00]">
+      <div className="flex flex-col justify-center items-start">
+        <p className="text-4xl font-semibold">...</p>
+        <p>Packages in-warehouse</p>
+      </div>
+      <div>
+        <Package size={96} />
+      </div>
+    </article>
+  )
+}
+
 export async function PackagesInWarehouseTile() {
   noStore()
   const [{ value }] = await db
