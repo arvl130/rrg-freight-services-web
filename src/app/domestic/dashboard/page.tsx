04b3ced9fb2308a48b342aa59f3327035b4fdf88
Arlene <138469304+gowbi@users.noreply.github.com>
2024-02-17 22:35:04 +0800

    fix: error domestic dashboard

--- src/app/domestic/dashboard/page.tsx
+++ src/app/domestic/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import { DomesticLayout } from "@/app/domestic/auth"
 import { useSession } from "@/hooks/session"
-
 import { CaretRight } from "@phosphor-icons/react/dist/ssr/CaretRight"
 import { api } from "@/utils/api"
 import { Package } from "@phosphor-icons/react/dist/icons/Package"
@@ -116,30 +115,6 @@ interface CalendarWidgetProps {
   month: string
 }
 
-function TotalArrivingShipmentTile() {
-  const { status, data } =
-    api.shipment.package.getTotalArrivingShipment.useQuery()
-  return (
-    <article
-      className=" text-[#AC873C]
-      grid grid-cols-[1fr_6rem] shadow-md px-8 py-6 rounded-lg
-      bg-gradient-to-b from-[#EDAD3E80] to-[#EDAD3E00]"
-    >
-      <div className="flex flex-col justify-center items-start">
-        <p className="text-4xl font-semibold"></p>
-        <p className="text-4xl font-semibold">
-          {status === "loading" && <>...</>}
-          {status === "error" && <>error</>}
-          {status === "success" && <>{data.count}</>}
-        </p>
-        <p>Total Arriving Shipment</p>
-      </div>
-      <div>
-        <Article size={96} />
-      </div>
-    </article>
-  )
-}
 function CalendarWidgetTile({ year, month }: CalendarWidgetProps) {
   function generateCalendarContent() {
     const daysInMonth = 31
