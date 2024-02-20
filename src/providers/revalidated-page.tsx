4fcd0c837c9d5622d75eec37758d344d17d17b1c
Angelo Geulin <angelogeulin123@gmail.com>
2024-02-20 21:36:19 +0800

    refactor: move to server dir

--- src/providers/revalidated-page.tsx
+++ src/providers/revalidated-page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { revalidatePath } from "@/actions/revalidate-path"
+import { revalidatePath } from "@/server/actions/revalidate-path"
 import { usePathname } from "next/navigation"
 import {
   ReactNode,
