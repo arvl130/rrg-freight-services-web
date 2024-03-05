3cc24a67c0d85fb24dee58a0ec38f4bb4fb5d9d4
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-05 21:24:19 +0800

    fix: use a more generic modal title

--- src/app/profile/change-password/edit-authenticator-modal.tsx
+++ src/app/profile/change-password/edit-authenticator-modal.tsx
@@ -99,7 +99,7 @@ export function EditModal({
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[min(calc(100%_-_3rem),_32rem)] grid grid-rows-[auto_1fr] rounded-2xl bg-white"
         >
           <Dialog.Title className="text-white font-bold text-center items-center py-2 [background-color:_#78CFDC] h-full rounded-t-2xl">
-            Edit device name
+            Edit Authenticator
           </Dialog.Title>
           <EditForm credential={credential} onClose={onClose} />
           <Dialog.Close asChild>
