976f0d2fcc264a50e2968752a45e97c9169d4003
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-06 13:24:24 +0800

    feat: add endpoint for retrieving all entities

--- src/app/api/backup/route.ts
+++ src/app/api/backup/route.ts
@@ -25,26 +25,28 @@ import type { NextRequest } from "next/server"
 import { z } from "zod"
 
 const inputSchema = z.object({
-  entity: z.union([
-    z.literal("USERS"),
-    z.literal("SHIPMENTS"),
-    z.literal("SHIPMENT_PACKAGES"),
-    z.literal("SHIPMENT_PACKAGE_OTPS"),
-    z.literal("SHIPMENT_LOCATIONS"),
-    z.literal("INCOMING_SHIPMENTS"),
-    z.literal("FORWARDER_TRANSFER_SHIPMENTS"),
-    z.literal("WAREHOUSE_TRANSFER_SHIPMENTS"),
-    z.literal("DELIVERY_SHIPMENTS"),
-    z.literal("WAREHOUSES"),
-    z.literal("VEHICLES"),
-    z.literal("PACKAGE_CATEGORIES"),
-    z.literal("PACKAGES"),
-    z.literal("PACKAGE_STATUS_LOGS"),
-    z.literal("ACTIVITIES"),
-    z.literal("WEBPUSH_SUBSCRIPTIONS"),
-    z.literal("WEBAUTHN_CHALLENGES"),
-    z.literal("WEBAUTHN_CREDENTIALS"),
-  ]),
+  entity: z
+    .union([
+      z.literal("USERS"),
+      z.literal("SHIPMENTS"),
+      z.literal("SHIPMENT_PACKAGES"),
+      z.literal("SHIPMENT_PACKAGE_OTPS"),
+      z.literal("SHIPMENT_LOCATIONS"),
+      z.literal("INCOMING_SHIPMENTS"),
+      z.literal("FORWARDER_TRANSFER_SHIPMENTS"),
+      z.literal("WAREHOUSE_TRANSFER_SHIPMENTS"),
+      z.literal("DELIVERY_SHIPMENTS"),
+      z.literal("WAREHOUSES"),
+      z.literal("VEHICLES"),
+      z.literal("PACKAGE_CATEGORIES"),
+      z.literal("PACKAGES"),
+      z.literal("PACKAGE_STATUS_LOGS"),
+      z.literal("ACTIVITIES"),
+      z.literal("WEBPUSH_SUBSCRIPTIONS"),
+      z.literal("WEBAUTHN_CHALLENGES"),
+      z.literal("WEBAUTHN_CREDENTIALS"),
+    ])
+    .nullable(),
 })
 
 export async function GET(req: NextRequest) {
@@ -253,6 +255,120 @@ export async function GET(req: NextRequest) {
     return Response.json({
       message: `Entity retrieved starting ${startAt}`,
       entity,
+    })
+  } else {
+    const activitiesResults = await db
+      .select()
+      .from(activities)
+      .where(gt(activities.createdAt, startAt))
+
+    const deliveryShipmentsResults = await db
+      .select()
+      .from(deliveryShipments)
+      .where(gt(deliveryShipments.createdAt, startAt))
+
+    const forwarderTransferShipmentsResults = await db
+      .select()
+      .from(forwarderTransferShipments)
+      .where(gt(forwarderTransferShipments.createdAt, startAt))
+
+    const packagesResults = await db
+      .select()
+      .from(packages)
+      .where(gt(packages.createdAt, startAt))
+
+    const packageCategoriesResults = await db
+      .select()
+      .from(packageCategories)
+      .where(gt(packageCategories.createdAt, startAt))
+
+    const packageStatusLogsResults = await db
+      .select()
+      .from(packageStatusLogs)
+      .where(gt(packageStatusLogs.createdAt, startAt))
+
+    const shipmentsResults = await db
+      .select()
+      .from(shipments)
+      .where(gt(shipments.createdAt, startAt))
+
+    const shipmentLocationsResults = await db
+      .select()
+      .from(shipmentLocations)
+      .where(gt(shipmentLocations.createdAt, startAt))
+
+    const shipmentPackagesResults = await db
+      .select()
+      .from(shipmentPackages)
+      .where(gt(shipmentPackages.createdAt, startAt))
+
+    const shipmentPackageOtpsResults = await db
+      .select()
+      .from(shipmentPackageOtps)
+      .where(gt(shipmentPackageOtps.createdAt, startAt))
+
+    const usersResults = await db
+      .select()
+      .from(users)
+      .where(gt(users.createdAt, startAt))
+
+    const vehiclesResults = await db
+      .select()
+      .from(vehicles)
+      .where(gt(vehicles.createdAt, startAt))
+
+    const warehousesResults = await db
+      .select()
+      .from(warehouses)
+      .where(gt(warehouses.createdAt, startAt))
+
+    const warehouseTransferShipmentsResults = await db
+      .select()
+      .from(warehouseTransferShipments)
+      .where(gt(warehouseTransferShipments.createdAt, startAt))
+
+    const webauthnChallengesResults = await db
+      .select()
+      .from(webauthnChallenges)
+      .where(gt(webauthnChallenges.createdAt, startAt))
+
+    const webauthnCredentialsResults = await db
+      .select()
+      .from(webauthnCredentials)
+      .where(gt(webauthnCredentials.createdAt, startAt))
+
+    const webpushSubscriptionsResults = await db
+      .select()
+      .from(webpushSubscriptions)
+      .where(gt(webpushSubscriptions.createdAt, startAt))
+
+    const incomingShipmentsResults = await db
+      .select()
+      .from(incomingShipments)
+      .where(gt(incomingShipments.createdAt, startAt))
+
+    return Response.json({
+      message: `Entity retrieved starting ${startAt}`,
+      entities: {
+        activities: activitiesResults,
+        deliveryShipments: deliveryShipmentsResults,
+        forwarderTransferShipments: forwarderTransferShipmentsResults,
+        packages: packagesResults,
+        packageCategories: packageCategoriesResults,
+        packageStatusLogs: packageStatusLogsResults,
+        shipments: shipmentsResults,
+        shipmentLocations: shipmentLocationsResults,
+        shipmentPackages: shipmentPackagesResults,
+        shipmentPackageOtps: shipmentPackageOtpsResults,
+        users: usersResults,
+        vehicles: vehiclesResults,
+        warehouses: warehousesResults,
+        warehouseTransferShipments: warehouseTransferShipmentsResults,
+        webauthnChallenges: webauthnChallengesResults,
+        webauthnCredentials: webauthnCredentialsResults,
+        webpushSubscriptions: webpushSubscriptionsResults,
+        incomingShipments: incomingShipmentsResults,
+      },
     })
   }
 }
