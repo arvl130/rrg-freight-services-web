4759b6371261c1d7bb8c964e741207ae4f95133d
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-27 20:14:47 +0800

    feat: include package count in db query result

--- src/app/api/v1/deliveries/route.ts
+++ src/app/api/v1/deliveries/route.ts
@@ -1,7 +1,12 @@
 import { validateSessionWithHeaders } from "@/server/auth"
 import { db } from "@/server/db/client"
-import { deliveryShipments, shipments } from "@/server/db/schema"
-import { eq } from "drizzle-orm"
+import {
+  deliveryShipments,
+  shipmentPackages,
+  shipments,
+} from "@/server/db/schema"
+import { eq, getTableColumns } from "drizzle-orm"
+import { sql } from "drizzle-orm"
 
 export async function GET(req: Request) {
   const { user } = await validateSessionWithHeaders({ req })
@@ -14,20 +19,26 @@ export async function GET(req: Request) {
     )
   }
 
-  const results = await db
-    .select()
+  const shipmentColumns = getTableColumns(shipments)
+  const { shipmentId, ...deliveryShipmentColumns } =
+    getTableColumns(deliveryShipments)
+
+  const deliveries = await db
+    .select({
+      ...shipmentColumns,
+      ...deliveryShipmentColumns,
+      packageCount: sql`count(${shipmentPackages.packageId})`.mapWith(Number),
+    })
     .from(deliveryShipments)
     .innerJoin(shipments, eq(deliveryShipments.shipmentId, shipments.id))
+    .innerJoin(
+      shipmentPackages,
+      eq(deliveryShipments.shipmentId, shipmentPackages.shipmentId),
+    )
+    .groupBy(deliveryShipments.shipmentId)
 
   return Response.json({
     message: "Deliveries retrieved",
-    deliveries: results.map(({ shipments, delivery_shipments }) => {
-      const { shipmentId, ...other } = delivery_shipments
-
-      return {
-        ...shipments,
-        ...other,
-      }
-    }),
+    deliveries,
   })
 }
