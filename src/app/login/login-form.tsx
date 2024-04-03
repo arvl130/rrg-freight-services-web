2fcf30fc9064d5d90b02aa30051f0141f6e3abdd
Angelo Geulin <angelogeulin123@gmail.com>
2024-04-03 17:17:19 +0800

    feat: swap login pages

--- src/app/login/login-form.tsx
+++ src/app/login/login-form.tsx
@@ -1,28 +1,46 @@
-"use client"
-
-import Link from "next/link"
+import { api } from "@/utils/api"
+import { startAuthentication } from "@simplewebauthn/browser"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useRef, useState, useTransition } from "react"
-import { Eye } from "@phosphor-icons/react/dist/ssr/Eye"
-import { EyeSlash } from "@phosphor-icons/react/dist/ssr/EyeSlash"
-import { signInWithEmailAndPasswordAction } from "./actions"
-import { useFormState } from "react-dom"
-import { formSchema } from "./form-schema"
 import type { z } from "zod"
+import { Fragment, useState } from "react"
+import { formSchema } from "./form-schema"
+import { signInWithWebauthnResponseAction } from "./actions"
+import toast from "react-hot-toast"
+import { type SavedUser } from "@/components/saved-users"
+import { useRouter } from "next/navigation"
+import Image from "next/image"
+import { UserCircle } from "@phosphor-icons/react/dist/ssr/UserCircle"
+import { Trash } from "@phosphor-icons/react/dist/ssr/Trash"
+import { CaretRight } from "@phosphor-icons/react/dist/ssr/CaretRight"
 
 type FormType = z.infer<typeof formSchema>
 
-export function LoginForm() {
-  const [state, formAction] = useFormState(signInWithEmailAndPasswordAction, {
-    message: "",
-  })
+function NoUsers(props: { onAddUser: (newUser: SavedUser) => void }) {
+  const router = useRouter()
 
-  const defaultValues = {
-    email: "",
-    password: "",
-    ...(state.fields ?? {}),
-  }
+  const generateAuthenticationOptionsMutation =
+    api.webauthn.generateAuthenticationOptions.useMutation({
+      onSuccess: async (options) => {
+        setIsSigningIn(true)
+        try {
+          const response = await startAuthentication(options)
+          const { data } = await signInWithWebauthnResponseAction(response)
+          if (data) {
+            router.replace(data.redirectPath)
+            props.onAddUser({
+              id: data.user.id,
+              email: data.user.email,
+              displayName: data.user.displayName,
+              photoUrl: data.user.photoUrl,
+            })
+          }
+        } catch {
+          setIsSigningIn(false)
+          toast.error("Sign in error occured.")
+        }
+      },
+    })
 
   const {
     register,
@@ -30,93 +48,225 @@ export function LoginForm() {
     formState: { errors },
   } = useForm<FormType>({
     resolver: zodResolver(formSchema),
-    defaultValues,
   })
 
-  const formRef = useRef<HTMLFormElement>(null)
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-  const [isPending, startTransition] = useTransition()
+  const [isSigningIn, setIsSigningIn] = useState(false)
 
   return (
     <form
-      ref={formRef}
-      action={formAction}
-      onSubmit={(e) => {
-        e.preventDefault()
-
-        handleSubmit(() => {
-          if (formRef.current) {
-            const formData = new FormData(formRef.current)
-            startTransition(() => {
-              formAction(formData)
-            })
-          }
-        })(e)
-      }}
+      onSubmit={handleSubmit(async (formData) => {
+        generateAuthenticationOptionsMutation.mutate({
+          email: formData.email,
+        })
+      })}
     >
       <div>
         <label className="font-medium block mb-1">Email</label>
         <input
-          type="email"
+          type="text"
           className="text-sm w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-          defaultValue={defaultValues.email}
           {...register("email")}
         />
         {errors.email && (
           <p className="text-red-600 mt-1 text-sm">{errors.email.message}</p>
         )}
       </div>
-      <div className="mt-4">
-        <label className="font-medium block mb-1">Password</label>
-        <div className="relative">
-          <input
-            type={isPasswordVisible ? "text" : "password"}
-            className="text-sm w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-            defaultValue={defaultValues.password}
-            {...register("password")}
-          />
-          <button
-            type="button"
-            className="absolute right-3 top-2.5"
-            onClick={() =>
-              setIsPasswordVisible(
-                (currentIsPasswordVisible) => !currentIsPasswordVisible,
-              )
-            }
-          >
-            {isPasswordVisible ? (
-              <Eye size={20} className="text-gray-600" />
-            ) : (
-              <EyeSlash size={20} className="text-gray-600" />
-            )}
-          </button>
-        </div>
-        {errors.password && (
-          <p className="text-red-600 mt-1 text-sm">{errors.password.message}</p>
-        )}
-      </div>
-      <div className="text-right mt-1">
-        <Link href="/forgot-password" className="text-sm">
-          Forgot Password?
-        </Link>
-      </div>
-      {state.message !== "" && !Array.isArray(state.issues) && (
-        <p className="text-red-600 text-center mt-3">{state.message}</p>
-      )}
-      {state.issues && (
-        <ul className="text-red-600 text-center mt-3">
-          {state.issues.map((issue, index) => (
-            <li key={index}>{issue}</li>
-          ))}
-        </ul>
-      )}
       <button
         type="submit"
+        disabled={
+          generateAuthenticationOptionsMutation.isLoading || isSigningIn
+        }
         className="font-semibold w-full mt-4 px-8 py-2.5 leading-5 text-white transition-colors duration-200 transform bg-brand-cyan-500 rounded-md hover:bg-brand-cyan-600 focus:outline-none focus:bg-brand-cyan-600 disabled:bg-brand-cyan-350"
-        disabled={isPending}
       >
         Sign in
       </button>
     </form>
+  )
+}
+
+function LoginWithSelection(props: {
+  savedUsers: SavedUser[]
+  onAddUser: (newUser: SavedUser) => void
+  onRemoveUser: (email: string) => void
+}) {
+  const [selectedUserId, setSelectedUserId] = useState(
+    props.savedUsers.length === 1 ? props.savedUsers[0].id : null,
+  )
+  const router = useRouter()
+
+  const generateAuthenticationOptionsMutation =
+    api.webauthn.generateAuthenticationOptions.useMutation({
+      onSuccess: async (options) => {
+        setIsSigningIn(true)
+        try {
+          const response = await startAuthentication(options)
+          const { data } = await signInWithWebauthnResponseAction(response)
+
+          if (data) {
+            router.replace(data.redirectPath)
+            props.onAddUser({
+              id: data.user.id,
+              email: data.user.email,
+              displayName: data.user.displayName,
+              photoUrl: data.user.photoUrl,
+            })
+          }
+        } catch {
+          setIsSigningIn(false)
+          toast.error("Sign in error occured.")
+        }
+      },
+    })
+
+  const [isSigningIn, setIsSigningIn] = useState(false)
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        const selectedUser = props.savedUsers.find(
+          ({ id }) => id === selectedUserId,
+        )
+
+        if (selectedUser)
+          generateAuthenticationOptionsMutation.mutate({
+            email: selectedUser.email,
+          })
+      }}
+    >
+      <div className="space-y-3">
+        {props.savedUsers.map((savedUser) => (
+          <div
+            key={savedUser.id}
+            className={`grid ${
+              savedUser.id === selectedUserId
+                ? "grid-cols-[3rem_1fr_3rem]"
+                : "grid-cols-[3rem_1fr]"
+            } gap-1`}
+          >
+            <button
+              type="button"
+              className={`grid grid-cols-subgrid col-span-2 border border-gray-300 ${
+                savedUser.id === selectedUserId ? "bg-gray-100" : ""
+              } transition-colors rounded-md`}
+              onClick={() => {
+                setSelectedUserId(savedUser.id)
+              }}
+            >
+              <div className="flex justify-center items-center pl-1 py-1">
+                {savedUser.photoUrl ? (
+                  <Image
+                    alt={`Photo of ${savedUser.displayName}`}
+                    src={savedUser.photoUrl}
+                    height={48}
+                    width={48}
+                    className="aspect-square rounded-full"
+                  />
+                ) : (
+                  <UserCircle size={48} />
+                )}
+              </div>
+              <div className="overflow-hidden p-1">
+                <p className="font-semibold">{savedUser.displayName}</p>
+                <p className="text-sm text-gray-500 overflow-hidden text-ellipsis">
+                  {savedUser.email}
+                </p>
+              </div>
+            </button>
+            {savedUser.id === selectedUserId && (
+              <div className="flex items-center justify-center">
+                <button
+                  type="button"
+                  className="border border-red-500 hover:bg-red-100 transition-colors p-1 rounded-md"
+                  onClick={() => {
+                    props.onRemoveUser(savedUser.id)
+                  }}
+                >
+                  <Trash size={24} className="text-red-500" />
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <button
+        type="submit"
+        disabled={
+          generateAuthenticationOptionsMutation.isLoading ||
+          isSigningIn ||
+          selectedUserId === null
+        }
+        className="font-semibold w-full mt-4 px-8 py-2.5 leading-5 text-white transition-colors duration-200 transform bg-brand-cyan-500 rounded-md hover:bg-brand-cyan-600 focus:outline-none focus:bg-brand-cyan-600 disabled:bg-brand-cyan-350"
+      >
+        Sign in
+      </button>
+    </form>
+  )
+}
+
+function HasUsers(props: {
+  savedUsers: SavedUser[]
+  onAddUser: (newUser: SavedUser) => void
+  onRemoveUser: (email: string) => void
+}) {
+  const [chooseUserFrom, setChooseUserFrom] = useState<"SELECTION" | "PROMPT">(
+    "SELECTION",
+  )
+
+  if (chooseUserFrom === "SELECTION")
+    return (
+      <>
+        <LoginWithSelection
+          savedUsers={props.savedUsers}
+          onAddUser={props.onAddUser}
+          onRemoveUser={props.onRemoveUser}
+        />
+        <div className="mt-3">
+          <button
+            type="button"
+            className="text-sm font-medium inline-flex items-center"
+            onClick={() => {
+              setChooseUserFrom("PROMPT")
+            }}
+          >
+            Login with a new user <CaretRight height={12} />
+          </button>
+        </div>
+      </>
+    )
+
+  return (
+    <>
+      <NoUsers onAddUser={props.onAddUser} />
+      <div className="mt-3">
+        <button
+          type="button"
+          className="text-sm font-medium inline-flex items-center"
+          onClick={() => {
+            setChooseUserFrom("SELECTION")
+          }}
+        >
+          Choose from remembered users <CaretRight height={12} />
+        </button>
+      </div>
+    </>
+  )
+}
+
+export function LoginForm(props: {
+  savedUsers: SavedUser[]
+  onAddUser: (newUser: SavedUser) => void
+  onRemoveUser: (id: string) => void
+}) {
+  if (props.savedUsers.length === 0)
+    return <NoUsers onAddUser={props.onAddUser} />
+
+  return (
+    <HasUsers
+      savedUsers={props.savedUsers}
+      onAddUser={props.onAddUser}
+      onRemoveUser={props.onRemoveUser}
+    />
   )
 }
--- src/app/login/with-email/page.tsx
+++ src/app/login/with-email/page.tsx
@@ -1,36 +1,23 @@
-"use client"
-
 import Image from "next/image"
 import Link from "next/link"
 import { CaretLeft } from "@phosphor-icons/react/dist/ssr/CaretLeft"
-import { useSavedUser } from "@/components/saved-users"
+import { validateSessionWithCookies } from "@/server/auth"
+import { redirect } from "next/navigation"
+import { getUserRoleRedirectPath } from "@/utils/redirects"
 import { LoginForm } from "./login-form"
-import { LoadingSpinner } from "@/components/spinner"
 
-export default function Page() {
-  const {
-    isLoading,
-    savedUsers,
-    addUser,
-    removeUserById: removeUserByEmail,
-  } = useSavedUser()
+export default async function LoginPage() {
+  const { user } = await validateSessionWithCookies()
+  if (user) {
+    const redirectPath = getUserRoleRedirectPath(user.role)
+    return redirect(redirectPath)
+  }
 
   return (
     <>
       <title>Login &#x2013; RRG Freight Services</title>
       <main className="min-h-dvh bg-brand-cyan-450 px-3 py-24 pb-3 relative">
-        <div className="absolute inset-0 h-full sm:grid grid-cols-2 hidden">
-          <div className="flex justify-center items-end">
-            <Image
-              src="/assets/img/login/right-bg.png"
-              alt="Ship, truck, and plane on a globe"
-              className="w-[500px] h-80 object-contain object-bottom"
-              width={500}
-              height={320}
-            />
-          </div>
-          <div></div>
-        </div>
+        <div className="absolute inset-0 h-full sm:grid grid-cols-2 hidden"></div>
         <section className="max-w-[26rem] mx-auto bg-white px-4 py-3 rounded-md shadow-lg relative">
           <div className="px-3 sm:px-6 pb-8">
             <div className="pb-6 pt-10">
@@ -47,25 +34,14 @@ export default function Page() {
                 Enter Your Credentials
               </p>
             </div>
-            {isLoading ? (
-              <div className="flex flex-col items-center justify-center">
-                <p className="mb-3 text-sm">Loading your options ...</p>
-                <LoadingSpinner />
-              </div>
-            ) : (
-              <LoginForm
-                savedUsers={savedUsers?.users ?? []}
-                onAddUser={addUser}
-                onRemoveUser={removeUserByEmail}
-              />
-            )}
+            <LoginForm />
           </div>
           <div className="text-sm flex justify-between">
             <Link href="/" className="inline-flex items-center">
               <CaretLeft height={12} /> Back to Homepage
             </Link>
 
-            <Link href="/login">Sign in with Email</Link>
+            <Link href="/login">Use an Authenticator</Link>
           </div>
         </section>
       </main>
