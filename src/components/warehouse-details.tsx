92062ea194d3e1abf08641005bdc52a63324ceff
Angelo Geulin <angelogeulin123@gmail.com>
2024-04-13 23:50:14 +0800

    feat: enable filters in Warehouse Staff Packages page

---
+++ src/components/warehouse-details.tsx
@@ -0,0 +1,12 @@
+import { api } from "@/utils/api"
+
+export function WarehouseDetails(props: { warehouseId: number }) {
+  const { status, data, error } = api.warehouse.getById.useQuery({
+    id: props.warehouseId,
+  })
+
+  if (status === "loading") return <>...</>
+  if (status === "error") return <>Error: {error.message}</>
+
+  return <>{data.displayName}</>
+}
