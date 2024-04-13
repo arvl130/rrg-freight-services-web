7430cbba6fedd68849448be61de65dc3dba77b6e
Angelo Geulin <angelogeulin123@gmail.com>
2024-04-14 00:44:21 +0800

    feat: add procedure for specific users

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -37,4 +37,124 @@ const hasUser = t.middleware(({ ctx: { user }, next }) => {
   })
 })
 
+const hasAdminUser = t.middleware(({ ctx: { user }, next }) => {
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  if (user.role !== "ADMIN") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      user: {
+        ...user,
+        role: user.role,
+      },
+    },
+  })
+})
+
+const hasWarehouseStaffUser = t.middleware(({ ctx: { user }, next }) => {
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  if (user.role !== "WAREHOUSE") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      user: {
+        ...user,
+        role: user.role,
+      },
+    },
+  })
+})
+
+const hasDriverUser = t.middleware(({ ctx: { user }, next }) => {
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  if (user.role !== "DRIVER") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      user: {
+        ...user,
+        role: user.role,
+      },
+    },
+  })
+})
+
+const hasOverseasAgentUser = t.middleware(({ ctx: { user }, next }) => {
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  if (user.role !== "OVERSEAS_AGENT") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      user: {
+        ...user,
+        role: user.role,
+      },
+    },
+  })
+})
+
+const hasDomesticAgentUser = t.middleware(({ ctx: { user }, next }) => {
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  if (user.role !== "DOMESTIC_AGENT") {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    })
+  }
+
+  return next({
+    ctx: {
+      user: {
+        ...user,
+        role: user.role,
+      },
+    },
+  })
+})
+
 export const protectedProcedure = t.procedure.use(hasUser)
+export const adminProcedure = t.procedure.use(hasAdminUser)
+export const warehouseStaffProcedure = t.procedure.use(hasWarehouseStaffUser)
+export const driverProcedure = t.procedure.use(hasDriverUser)
+export const overseasAgentProcedure = t.procedure.use(hasOverseasAgentUser)
+export const domesticAgentProcedure = t.procedure.use(hasDomesticAgentUser)
