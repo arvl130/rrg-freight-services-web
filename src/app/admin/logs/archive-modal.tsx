31f19ec3622e2413e38fc917d721ec8082c6c0fd
Angelo Geulin <angelogeulin123@gmail.com>
2024-04-21 15:36:57 +0800

    feat: add archive/unarchive form to packages and activity logs

---
+++ src/app/admin/logs/archive-modal.tsx
@@ -0,0 +1,85 @@
+import type { Activity } from "@/server/db/entities"
+import { api } from "@/utils/api"
+import { X } from "@phosphor-icons/react/dist/ssr/X"
+import * as Dialog from "@radix-ui/react-dialog"
+import toast from "react-hot-toast"
+
+function UpdateForm({ item, close }: { item: Activity; close: () => void }) {
+  const apiUtils = api.useUtils()
+  const { mutate, isLoading } = api.activity.archiveById.useMutation({
+    onSuccess: () => {
+      apiUtils.activity.getAll.invalidate()
+      close()
+      toast.success("Activity log archived.")
+    },
+  })
+
+  return (
+    <form
+      className="px-4 pt-2 pb-4"
+      onSubmit={(e) => {
+        e.preventDefault()
+        mutate({
+          id: item.id,
+        })
+      }}
+    >
+      <div className="mb-3">
+        Are you sure you want to archive this activity log?{" "}
+        <span className="font-medium">{item.id}</span>
+      </div>
+      <div className="flex justify-end">
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-400 transition-colors duration-200 disabled:bg-blue-300 rounded-md text-white font-medium"
+          disabled={isLoading}
+        >
+          Archive
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export function ArchiveModal({
+  id,
+  isOpen,
+  close,
+}: {
+  id: number
+  isOpen: boolean
+  close: () => void
+}) {
+  const { status, data, error } = api.activity.getById.useQuery({
+    id,
+  })
+
+  return (
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/40 fixed inset-0" onClick={close} />
+        <Dialog.Content
+          onEscapeKeyDown={close}
+          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[min(calc(100%_-_3rem),_32rem)] grid grid-rows-[auto_1fr] rounded-2xl bg-white"
+        >
+          <Dialog.Title className="text-white font-bold text-center items-center py-2 [background-color:_#78CFDC] h-full rounded-t-2xl">
+            Archive Activity Log
+          </Dialog.Title>
+          {status === "loading" && <div>Loading ...</div>}
+          {status === "error" && <div>Error occured: {error.message}</div>}
+          {status === "success" && <UpdateForm item={data} close={close} />}
+          <Dialog.Close asChild>
+            <button
+              type="button"
+              className="text-white absolute top-3 right-3"
+              onClick={close}
+            >
+              <X size={20} />
+              <span className="sr-only">Close</span>
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
--- src/app/admin/logs/main-section.tsx
+++ src/app/admin/logs/main-section.tsx
@@ -4,14 +4,22 @@ import { api } from "@/utils/api"
 import { useState } from "react"
 import { LoadingSpinner } from "@/components/spinner"
 import * as Table from "@/components/table"
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 import { usePaginatedItems } from "@/hooks/paginated-items"
 import type { Activity } from "@/server/db/entities"
 import { getColorFromActivityVerb } from "@/utils/colors"
 import { SUPPORTED_ACTIVITY_VERB, type ActivityVerb } from "@/utils/constants"
+import { DotsThree } from "@phosphor-icons/react/dist/ssr/DotsThree"
+import { ArchiveModal } from "./archive-modal"
+import { UnarchiveModal } from "./unarchive-modal"
 
 type ActivityWithUserDisplayName = Activity & { createdByDisplayName: string }
 
 function TableItem({ item }: { item: ActivityWithUserDisplayName }) {
+  const [visibleModal, setVisibleModal] = useState<
+    null | "ARCHIVE" | "UNARCHIVE"
+  >(null)
+
   return (
     <>
       <div className="px-4 py-2 border-b border-gray-300 text-sm text-right">
@@ -34,6 +42,49 @@ function TableItem({ item }: { item: ActivityWithUserDisplayName }) {
       </div>
       <div className="px-4 py-2 border-b border-gray-300 text-sm">
         {item.entity}
+      </div>
+      <div className="px-4 py-2 border-b border-gray-300 text-sm">
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button type="button">
+              <span className="sr-only">Actions</span>
+              <DotsThree size={16} />
+            </button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content className="bg-white rounded-lg drop-shadow-lg text-sm">
+              {item.isArchived ? (
+                <DropdownMenu.Item
+                  className="transition-colors rounded-lg hover:bg-sky-50 px-3 py-2"
+                  onClick={() => setVisibleModal("UNARCHIVE")}
+                >
+                  Unarchive
+                </DropdownMenu.Item>
+              ) : (
+                <DropdownMenu.Item
+                  className="transition-colors rounded-lg hover:bg-sky-50 px-3 py-2"
+                  onClick={() => setVisibleModal("ARCHIVE")}
+                >
+                  Archive
+                </DropdownMenu.Item>
+              )}
+
+              <DropdownMenu.Arrow className="fill-white" />
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+
+        <ArchiveModal
+          id={item.id}
+          close={() => setVisibleModal(null)}
+          isOpen={visibleModal === "ARCHIVE"}
+        />
+        <UnarchiveModal
+          id={item.id}
+          close={() => setVisibleModal(null)}
+          isOpen={visibleModal === "UNARCHIVE"}
+        />
       </div>
     </>
   )
@@ -165,7 +216,7 @@ function ActivitiesTable({ items }: { items: ActivityWithUserDisplayName[] }) {
             gotoPreviousPage={gotoPreviousPage}
           />
         </div>
-        <div className="grid grid-cols-[repeat(4,_auto)_1fr] auto-rows-min overflow-auto">
+        <div className="grid grid-cols-[repeat(5,_auto)_1fr] auto-rows-min overflow-auto">
           <div className="uppercase px-4 py-2 border-y border-gray-300 font-medium">
             ID
           </div>
@@ -181,8 +232,11 @@ function ActivitiesTable({ items }: { items: ActivityWithUserDisplayName[] }) {
           <div className="uppercase px-4 py-2 border-y border-gray-300 font-medium">
             Entity
           </div>
+          <div className="uppercase px-4 py-2 border-y border-gray-300 font-medium">
+            Actions
+          </div>
           {paginatedItems.length === 0 ? (
-            <div className="text-center pt-4 col-span-5">
+            <div className="text-center pt-4 col-span-6">
               No activity found.
             </div>
           ) : (
