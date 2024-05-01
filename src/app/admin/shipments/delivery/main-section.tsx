ec425df4e53985befebbc856160c87032ef3c7b6
Wally Gaynor <walliegaynor29@gmail.com>
2024-05-01 18:23:07 +0800

    fix: allow archiving only for complete shipments & delivered or transferred packages

--- src/app/admin/shipments/delivery/main-section.tsx
+++ src/app/admin/shipments/delivery/main-section.tsx
@@ -110,21 +110,7 @@ function TableItem({ item }: { item: NormalizedDeliveryShipmentWithDetails }) {
               >
                 View Locations
               </DropdownMenu.Item>
-              {/* {item.isArchived ? (
-                <DropdownMenu.Item
-                  className="transition-colors rounded-b-lg hover:bg-sky-50 px-3 py-2"
-                  onClick={() => setVisibleModal("UNARCHIVE")}
-                >
-                  Unarchive
-                </DropdownMenu.Item>
-              ) : (
-                <DropdownMenu.Item
-                  className="transition-colors rounded-b-lg hover:bg-sky-50 px-3 py-2"
-                  onClick={() => setVisibleModal("ARCHIVE")}
-                >
-                  Archive
-                </DropdownMenu.Item>
-              )} */}
+
               {item.status === "COMPLETED" ? (
                 item.isArchived ? (
                   <DropdownMenu.Item
