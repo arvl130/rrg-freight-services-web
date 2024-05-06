315f2dc6b547085fe739e9f65054802064b85a71
Wally Gaynor <walliegaynor29@gmail.com>
2024-05-06 23:48:17 +0800

    fix: warehouse capacity % to 2 decimal & show message when no packages in modal

--- src/components/shipments/view-details-modal.tsx
+++ src/components/shipments/view-details-modal.tsx
@@ -50,59 +50,70 @@ export function ViewDetailsModal({
           </Dialog.Title>
           {status === "loading" && <div>loading ...</div>}
           {status === "error" && <div>An error occured: {error.message}</div>}
+
           {status === "success" && (
-            <div className="overflow-auto">
-              {packages.map((_package) => (
-                <div
-                  key={_package.id}
-                  className="bg-gray-100 grid grid-cols-[1rem_1fr] text-sm"
-                >
-                  <div
-                    className={`${getColorFromPackageStatus(_package.status)}`}
-                  />
-                  <div className="border-b border-gray-300 px-3 py-1">
-                    <div className="grid grid-cols-[6rem_1fr]">
-                      <div className="font-semibold">Package ID:</div>
-                      <div>{_package.id}</div>
-                    </div>
-                    <div className="grid grid-cols-[6rem_1fr]">
-                      <div className="font-semibold">Status:</div>
-                      <div>
-                        <span
-                          className={`inline-block text-white px-2 rounded-full ${getColorFromPackageStatus(
-                            _package.status,
-                          )}`}
-                        >
-                          {getHumanizedOfPackageStatus(_package.status)}
-                        </span>
+            <>
+              {packages.length !== 0 ? (
+                <div className="overflow-auto">
+                  {packages.map((_package) => (
+                    <div
+                      key={_package.id}
+                      className="bg-gray-100 grid grid-cols-[1rem_1fr] text-sm"
+                    >
+                      <div
+                        className={`${getColorFromPackageStatus(
+                          _package.status,
+                        )}`}
+                      />
+                      <div className="border-b border-gray-300 px-3 py-1">
+                        <div className="grid grid-cols-[6rem_1fr]">
+                          <div className="font-semibold">Package ID:</div>
+                          <div>{_package.id}</div>
+                        </div>
+                        <div className="grid grid-cols-[6rem_1fr]">
+                          <div className="font-semibold">Status:</div>
+                          <div>
+                            <span
+                              className={`inline-block text-white px-2 rounded-full ${getColorFromPackageStatus(
+                                _package.status,
+                              )}`}
+                            >
+                              {getHumanizedOfPackageStatus(_package.status)}
+                            </span>
+                          </div>
+                        </div>
+                        <div className="grid grid-cols-[6rem_1fr]">
+                          <div className="font-semibold">Sender:</div>
+                          <div>{_package.senderFullName}</div>
+                        </div>
+                        <div className="grid grid-cols-[6rem_1fr]">
+                          <div className="font-semibold">Receiver:</div>
+                          <div>{_package.receiverFullName}</div>
+                        </div>
+                        <div className="grid grid-cols-[6rem_1fr]">
+                          <div className="font-semibold">Address:</div>
+                          <div>
+                            <p>
+                              {_package.receiverStreetAddress},{" "}
+                              {_package.receiverBarangay},{" "}
+                              {_package.receiverCity}
+                            </p>
+                            <p>
+                              {_package.receiverStateOrProvince}{" "}
+                              {_package.receiverPostalCode}
+                            </p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="grid grid-cols-[6rem_1fr]">
-                      <div className="font-semibold">Sender:</div>
-                      <div>{_package.senderFullName}</div>
-                    </div>
-                    <div className="grid grid-cols-[6rem_1fr]">
-                      <div className="font-semibold">Receiver:</div>
-                      <div>{_package.receiverFullName}</div>
-                    </div>
-                    <div className="grid grid-cols-[6rem_1fr]">
-                      <div className="font-semibold">Address:</div>
-                      <div>
-                        <p>
-                          {_package.receiverStreetAddress},{" "}
-                          {_package.receiverBarangay}, {_package.receiverCity}
-                        </p>
-                        <p>
-                          {_package.receiverStateOrProvince}{" "}
-                          {_package.receiverPostalCode}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              ) : (
+                <div className="text-center py-5">No Packages Found.</div>
+              )}
+            </>
           )}
+
           <div className=" mt-2 px-5">
             <Dialog.Close asChild>
               <button className="mb-3" onClick={close}>
